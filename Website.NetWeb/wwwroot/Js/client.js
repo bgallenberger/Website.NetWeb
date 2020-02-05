58391c3eeca5b7378fddb2ec69ca21dd93fbdfde
bgallenberger <54650768+bgallenberger@users.noreply.github.com>
2020-02-05 12:46:40 -0600

    Add files via upload
    remaking the repository

---
+++ Website.NetWeb/wwwroot/Js/client.js
@@ -0,0 +1,25 @@
+﻿$(function () {
+    // preload audio
+    var toast = new Audio('media/toast.wav');
+
+    $('.code').on('click', function (e) {
+        e.preventDefault();
+        //change values in box
+        $('#product').html($(this).data('product'));
+        $('#code').html($(this).data('code'));
+        $('#toast').toast({ autohide: false }).toast('show');
+        // first pause the audio (in case it is still playing)
+        toast.pause();
+        // reset the audio
+        toast.currentTime = 0;
+        // play audio
+        toast.play();
+        $('#toast').toast({ autohide: false }).toast('show');
+    });
+    //close on esc key press
+    $(document).on('keyup', function (e) {
+        if (e.key === "Escape") {
+            $('#toast').toast('hide');
+        }
+    });
+});
